76e844b761db921d3d7937fa2dea578f9005fcb0
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-11-26 20:10:13 +0100

    Dodavanje grouBoxa

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("unosStudenta.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Formular za unos studenta");
+        primaryStage.setScene(new Scene(root, 626.0, 663.0));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
