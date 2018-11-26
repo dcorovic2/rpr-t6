a9f6babe92c72b38cdeeb8d19eba6d7b2e773344
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-11-26 23:28:38 +0100

    Ostale validacije

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -100,4 +100,84 @@ public class Controller {
             }
         });
     }
+
+    public void unosTelefona(KeyEvent keyEvent) {
+        konTel.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                Validacija e = new Validacija();
+                if (e.validnoTelefon(n)) {
+                    konTel.getStyleClass().removeAll("poljeNijeIspravno");
+                    konTel.getStyleClass().add("poljeIspravno");
+                } else {
+                    konTel.getStyleClass().removeAll("poljeIspravno");
+                    konTel.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
+    public void unosOdsjeka(KeyEvent keyEvent) {
+        odjesk.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                Validacija e = new Validacija();
+                if (e.validnoOdsjek(n)) {
+                    odjesk.getStyleClass().removeAll("poljeNijeIspravno");
+                    odjesk.getStyleClass().add("poljeIspravno");
+                } else {
+                    odjesk.getStyleClass().removeAll("poljeIspravno");
+                    odjesk.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
+    public void unosGodine(KeyEvent keyEvent) {
+        godina.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                Validacija e = new Validacija();
+                if (e.validnoGodine(n)) {
+                    godina.getStyleClass().removeAll("poljeNijeIspravno");
+                    godina.getStyleClass().add("poljeIspravno");
+                } else {
+                    godina.getStyleClass().removeAll("poljeIspravno");
+                    godina.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
+    public void unosCiklusa(KeyEvent keyEvent) {
+        ciklus.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                Validacija e = new Validacija();
+                if (e.validnoCiklus(n)) {
+                    ciklus.getStyleClass().removeAll("poljeNijeIspravno");
+                    ciklus.getStyleClass().add("poljeIspravno");
+                } else {
+                    ciklus.getStyleClass().removeAll("poljeIspravno");
+                    ciklus.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
+    public void unosMaila(KeyEvent keyEvent) {
+        mailAdresa.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                Validacija e = new Validacija();
+                if (e.validnoMail(n)) {
+                    mailAdresa.getStyleClass().removeAll("poljeNijeIspravno");
+                    mailAdresa.getStyleClass().add("poljeIspravno");
+                } else {
+                    mailAdresa.getStyleClass().removeAll("poljeIspravno");
+                    mailAdresa.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+    }
 }
--- src/sample/Validacija.java
+++ src/sample/Validacija.java
@@ -21,4 +21,36 @@ public class Validacija {
         }
         return false;
     }
+
+    public boolean validnoTelefon(String n) {
+        if(n.length()>0 && n.length()<=9){
+            try{
+                Integer.parseInt(n);
+            } catch (NumberFormatException e){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validnoOdsjek(String n) {
+        if(n.equals("EE") || n.equals("RI") || n.equals("AE") || n.equals("TK")) return true;
+        return false;
+    }
+
+    public boolean validnoGodine(String n) {
+        if(n.equals("prva") || n.equals("druga") || n.equals("treca")) return true;
+        return false;
+    }
+
+    public boolean validnoCiklus(String n) {
+        if(n.equals("bachelor") || n.equals("master") || n.equals("doktorski ciklus") || n.equals("strucni studij")) return true;
+        return false;
+    }
+
+    public boolean validnoMail(String n) {
+        if(n.length()>0 && n.contains("@") && (n.contains("com") || n.contains("ba"))) return true;
+        return false;
+    }
 }
