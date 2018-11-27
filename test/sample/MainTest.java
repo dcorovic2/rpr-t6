85690ef302ef9b1505d81f8bcdeab8c87aa71135
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-11-27 16:33:11 +0100

    Dodavanje direktorija za testove

---
+++ test/sample/MainTest.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import org.junit.jupiter.api.extension.ExtendWith;
+/*
+@ExtendWith(ApplicationExtension.class)
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void start() throws Exception{
+        Parent mainNode = FXMLLoader.load(Main.class.getResource("unosStudenta.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}*/
