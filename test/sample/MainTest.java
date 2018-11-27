5a995144e5b81ee53b21bc692ba0f2c6e4c28f74
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-11-27 17:16:09 +0100

    Dodavanje direktorija za testove

--- test/sample/MainTest.java
+++ test/sample/MainTest.java
@@ -1,22 +1,37 @@
 package sample;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.sun.javafx.robot.FXRobot;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-/*
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.awt.*;
+
 @ExtendWith(ApplicationExtension.class)
 class MainTest {
 
-    @org.junit.jupiter.api.Test
-    void start() throws Exception{
+    private TextArea ime;
+
+    @Start
+    void start(Stage stage) throws Exception{
         Parent mainNode = FXMLLoader.load(Main.class.getResource("unosStudenta.fxml"));
         stage.setScene(new Scene(mainNode));
         stage.show();
         stage.toFront();
     }
 
-    @org.junit.jupiter.api.Test
-    void main() {
+    @Test
+    void klikPrijava(FXRobot robot) {
+        ime = robot.lookup("#imeField").queryAs(TextArea.class);
+        ime.setText(ime.getText() + robot.keyPress(KeyCode.D));
     }
-}*/
+}
